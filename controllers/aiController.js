21c1acb5176c996c32b0d2cac396ae0fe89fdc07
lotusjoshigehu <joshikamal626@gmail.com>
2026-06-11 21:59:29 +0530

    Update aiController.js

--- controllers/aiController.js
+++ controllers/aiController.js
@@ -3,16 +3,24 @@ const { askAI } = require("../services/aiservices");
 async function askQuestion(req, res) {
   try {
     const { question } = req.body;
-    if (!question) return res.status(400).json({ message: "Question required" });
+
+    if (!question) {
+      return res.status(400).json({
+        message: "Question required"
+      });
+    }
 
     const answer = await askAI(question);
+
     res.json({ answer });
-  } catch {
+
+  } catch (error) {
     console.error("AI Error:", error);
-  res.status(500).json({
-    message: "AI failed",
-    error: error.message
-  });
+
+    res.status(500).json({
+      message: "AI failed",
+      error: error.message
+    });
   }
 }
 
