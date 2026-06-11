87de67474948c9e8e7d51ccdd799e16952e4ba04
lotusjoshigehu <joshikamal626@gmail.com>
2026-06-11 20:13:36 +0530

    Update login API endpoint to production URL

--- login.js
+++ login.js
@@ -6,7 +6,7 @@ document.getElementById("loginForm").addEventListener("submit", async e => {
     const password = document.getElementById("password").value;
 
     try {
-        const res = await fetch("http://localhost:3000/login", {
+        const res = await fetch("https://s-graphene.onrender.com/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ email, password })
