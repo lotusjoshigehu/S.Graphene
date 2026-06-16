77dcbfc628afd4871530a731e2a9f97786746b97
lotusjoshigehu <joshikamal626@gmail.com>
2026-06-16 10:29:50 +0530

    Refactor expense controller functions

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -6,13 +6,7 @@ async function addExpense(req, res) {
     const t = await sequelize.transaction();
 
     try {
-        const {
-            email,
-            amount,
-            category,
-            date,
-            note
-        } = req.body;
+        const { email, amount, category, date, note } = req.body;
 
         const user = await User.findOne({
             where: { email },
@@ -30,42 +24,28 @@ async function addExpense(req, res) {
             date,
             note,
             UserId: user.id
-        }, {
-            transaction: t
-        });
+        }, { transaction: t });
 
         user.totalExpense += Number(amount);
-
-        await user.save({
-            transaction: t
-        });
+        await user.save({ transaction: t });
 
         await t.commit();
-
         res.json(expense);
 
     } catch (err) {
-
         await t.rollback();
-
         res.status(500).json(err.message);
     }
 }
 
 async function getExpenses(req, res) {
-
     try {
-
         const user = await User.findOne({
-            where: {
-                email: req.params.email
-            },
+            where: { email: req.params.email },
             include: Expense
         });
 
-        if (!user) {
-            return res.json([]);
-        }
+        if (!user) return res.json([]);
 
         const expenses = user.Expenses.sort(
             (a, b) => new Date(b.date) - new Date(a.date)
@@ -74,80 +54,50 @@ async function getExpenses(req, res) {
         res.json(expenses);
 
     } catch (err) {
-
         res.status(500).json(err.message);
     }
 }
 
 async function deleteExpense(req, res) {
-
     const t = await sequelize.transaction();
 
     try {
-
-        const expense = await Expense.findByPk(
-            req.params.id,
-            { transaction: t }
-        );
+        const expense = await Expense.findByPk(req.params.id, {
+            transaction: t
+        });
 
         if (!expense) {
-
             await t.rollback();
-
-            return res.status(404).json(
-                "Expense not found"
-            );
+            return res.status(404).json("Expense not found");
         }
 
-        const user = await User.findByPk(
-            expense.UserId,
-            { transaction: t }
-        );
-
-        user.totalExpense -= Number(
-            expense.amount
-        );
-
-        await user.save({
+        const user = await User.findByPk(expense.UserId, {
             transaction: t
         });
 
-        await expense.destroy({
-            transaction: t
-        });
+        user.totalExpense -= Number(expense.amount);
+
+        await user.save({ transaction: t });+
+        await expense.destroy({ transaction: t });
 
         await t.commit();
-
         res.json("Deleted");
 
     } catch (err) {
-
         await t.rollback();
-
-        res.status(500).json(
-            err.message
-        );
+        res.status(500).json(err.message);
     }
 }
 
 async function updateExpense(req, res) {
-
     try {
 
-        const {
-            amount,
-            category,
-            date,
-            note
-        } = req.body;
+        const { amount, category, date, note } = req.body;
 
         const expense =
-            await Expense.findByPk(
-                req.params.id
-            );
+            await Expense.findByPk(req.params.id);
 
         if (!expense) {
-
             return res.status(404).json(
                 "Expense not found"
             );
